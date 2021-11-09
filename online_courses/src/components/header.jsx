36cd20667e6150d353268d170c33baee1ffb7d9f
Leonid <orlan1211@yahoo.com>
2021-11-09 17:09:27 +0300

    Pre-core assembly of routing components.

---
+++ online_courses/src/components/header.jsx
@@ -0,0 +1,15 @@
+/**
+ * Create by Kuznetsov Leonid 
+ * 09.11.2021
+ */ 
+import React from 'react';
+
+const header = () =>{
+    return (
+        <>
+            <div>hello world</div>
+        </>
+    )
+}
+
+export default header
---
+++ online_courses/src/pajes/wiew.js
@@ -0,0 +1,20 @@
+/**
+ * Create by Kuznetsov Leonid 
+ * 09.11.2021
+ */ 
+
+//Pre-core assembly of routing components.
+
+import React from 'react';
+import Header from '../components/header';
+import Fouter from '../components/fouter';
+
+const wiew = () =>{
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default wiew
